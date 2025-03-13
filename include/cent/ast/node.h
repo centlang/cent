83eedc2920f37cd6d447d08389b06a5e1f19d486
lnkrr <lnkrr@proton.me>
2025-03-13 15:05:50 +0300

    feat(ast): define Node

---
+++ include/cent/ast/node.h
@@ -0,0 +1,31 @@
+#ifndef CENT_AST_NODE_H
+#define CENT_AST_NODE_H
+
+#include "cent/span.h"
+
+namespace cent {
+
+struct Node {
+    [[nodiscard]] Node() noexcept = default;
+    virtual ~Node() noexcept = default;
+
+    Node(const Node&) = delete;
+    Node(Node&&) = delete;
+
+    auto operator=(const Node&) = delete;
+    auto operator=(Node&&) = delete;
+};
+
+struct Statement : Node {
+    [[nodiscard]] Statement(Span span) noexcept : span{span} {}
+
+    Span span;
+};
+
+struct Expression : Statement {
+    using Statement::Statement;
+};
+
+} // namespace cent
+
+#endif
