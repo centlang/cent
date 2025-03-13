1aed4699bda5e94122027e83d940affa4384c09e
lnkrr <lnkrr@proton.me>
2025-03-13 15:53:18 +0300

    feat(ast): define BinaryExpr

---
+++ include/cent/ast/binary_expr.h
@@ -0,0 +1,29 @@
+#ifndef CENT_AST_BINARY_EXPR_H
+#define CENT_AST_BINARY_EXPR_H
+
+#include <memory>
+#include <utility>
+
+#include "cent/frontend/token.h"
+#include "cent/span.h"
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct BinaryExpr : Expression {
+    [[nodiscard]] BinaryExpr(
+        Span span, SpanValue<Token::Type> oper, std::unique_ptr<Expression> lhs,
+        std::unique_ptr<Expression> rhs) noexcept
+    : Expression{span}, oper{oper}, lhs{std::move(lhs)}, rhs{std::move(rhs)} {}
+
+    SpanValue<Token::Type> oper;
+
+    std::unique_ptr<Expression> lhs;
+    std::unique_ptr<Expression> rhs;
+};
+
+} // namespace cent
+
+#endif
---
+++ include/cent/ast/span_value.h
@@ -0,0 +1,15 @@
+#ifndef CENT_AST_SPAN_VALUE_H
+#define CENT_AST_SPAN_VALUE_H
+
+#include "cent/span.h"
+
+namespace cent {
+
+template <typename ValueType> struct SpanValue {
+    ValueType value;
+    Span span;
+};
+
+} // namespace cent
+
+#endif
