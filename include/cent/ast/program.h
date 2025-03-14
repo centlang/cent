d481a1a0243d9dc4505543b814f7d7cb807977b2
lnkrr <lnkrr@proton.me>
2025-03-14 15:58:44 +0300

    feat(ast): define Program

---
+++ include/cent/ast/program.h
@@ -0,0 +1,22 @@
+#ifndef CENT_AST_PROGRAM_H
+#define CENT_AST_PROGRAM_H
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "cent/ast/fn_decl.h"
+
+namespace cent {
+
+struct Program : Node {
+    [[nodiscard]] Program(
+        std::vector<std::unique_ptr<FnDecl>> functions = {}) noexcept
+    : functions{std::move(functions)} {}
+
+    std::vector<std::unique_ptr<FnDecl>> functions;
+};
+
+} // namespace cent
+
+#endif
