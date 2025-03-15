86f3bb7c1ef1d45ea12c26eedd6f8d1b3c7cf62e
lnkrr <lnkrr@proton.me>
2025-03-15 15:42:07 +0300

    feat(ast): define UnaryExpr

---
+++ include/cent/ast/unary_expr.h
@@ -0,0 +1,27 @@
+#ifndef CENT_AST_UNARY_EXPR_H
+#define CENT_AST_UNARY_EXPR_H
+
+#include <memory>
+#include <utility>
+
+#include "cent/frontend/token.h"
+#include "cent/span.h"
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct UnaryExpr : Expression {
+    [[nodiscard]] UnaryExpr(
+        Span span, SpanValue<Token::Type> oper,
+        std::unique_ptr<Expression> value) noexcept
+    : Expression{span}, oper{oper}, value{std::move(value)} {}
+
+    SpanValue<Token::Type> oper;
+    std::unique_ptr<Expression> value;
+};
+
+} // namespace cent
+
+#endif
