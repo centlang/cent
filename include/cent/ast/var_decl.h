87abcfe5aeb51ec67f32f6c39564ce0d377000d5
lnkrr <lnkrr@proton.me>
2025-03-18 16:14:35 +0300

    feat(ast): define VarDecl

---
+++ include/cent/ast/var_decl.h
@@ -0,0 +1,33 @@
+#ifndef CENT_AST_VAR_DECL_H
+#define CENT_AST_VAR_DECL_H
+
+#include <memory>
+#include <string_view>
+#include <utility>
+
+#include "cent/span.h"
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct VarDecl : Declaration {
+    [[nodiscard]] VarDecl(
+        Span span, bool is_mutable, SpanValue<std::string_view> name,
+        SpanValue<std::string_view> type,
+        std::unique_ptr<Expression> value) noexcept
+    : Declaration{span}, is_mutable{is_mutable}, name{name}, type{type},
+      value{std::move(value)} {}
+
+    bool is_mutable;
+
+    SpanValue<std::string_view> name;
+    SpanValue<std::string_view> type;
+
+    std::unique_ptr<Expression> value;
+};
+
+} // namespace cent
+
+#endif
