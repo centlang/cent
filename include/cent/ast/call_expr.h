c6c04415abe9ef8b70c04d03cea059eccad1313e
lnkrr <lnkrr@proton.me>
2025-03-22 14:23:00 +0300

    feat(ast): define CallExpr

---
+++ include/cent/ast/call_expr.h
@@ -0,0 +1,27 @@
+#ifndef CENT_AST_CALL_EXPR_H
+#define CENT_AST_CALL_EXPR_H
+
+#include <memory>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct CallExpr : Expression {
+    [[nodiscard]] CallExpr(
+        Span span, SpanValue<std::string_view> identifier,
+        std::vector<std::unique_ptr<Expression>> arguments) noexcept
+    : Expression{span}, identifier{identifier},
+      arguments{std::move(arguments)} {}
+
+    SpanValue<std::string_view> identifier;
+    std::vector<std::unique_ptr<Expression>> arguments;
+};
+
+} // namespace cent
+
+#endif
