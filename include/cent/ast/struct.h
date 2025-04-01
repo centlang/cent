e53e5e0b725ff2925d02bda5efc62a880fa5be53
lnkrr <lnkrr@proton.me>
2025-04-01 16:33:18 +0300

    feat(ast): define Struct

---
+++ include/cent/ast/struct.h
@@ -0,0 +1,29 @@
+#ifndef CENT_AST_STRUCT_H
+#define CENT_AST_STRUCT_H
+
+#include <string_view>
+#include <vector>
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct Struct : detail::Decl<Struct> {
+    struct Field {
+        SpanValue<std::string_view> name;
+        SpanValue<std::string_view> type;
+    };
+
+    [[nodiscard]] Struct(
+        Span span, SpanValue<std::string_view> name,
+        std::vector<Field> fields) noexcept
+    : Decl{span}, name{name}, fields{std::move(fields)} {}
+
+    SpanValue<std::string_view> name;
+    std::vector<Field> fields;
+};
+
+} // namespace cent
+
+#endif
