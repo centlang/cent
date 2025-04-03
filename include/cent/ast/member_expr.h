687fb6351f75aa15a62addbcc931e1039f0d50ac
lnkrr <lnkrr@proton.me>
2025-04-03 14:19:00 +0300

    feat(ast): define MemberExpr

---
+++ include/cent/ast/member_expr.h
@@ -0,0 +1,26 @@
+#ifndef CENT_AST_MEMBER_EXPR_H
+#define CENT_AST_MEMBER_EXPR_H
+
+#include <memory>
+#include <string_view>
+
+#include "cent/span.h"
+
+#include "cent/ast/node.h"
+#include "cent/ast/span_value.h"
+
+namespace cent {
+
+struct MemberExpr : detail::Expr<MemberExpr> {
+    [[nodiscard]] MemberExpr(
+        Span span, std::unique_ptr<Expression> parent,
+        SpanValue<std::string_view> member) noexcept
+    : Expr{span}, parent{std::move(parent)}, member{member} {}
+
+    std::unique_ptr<Expression> parent;
+    SpanValue<std::string_view> member;
+};
+
+} // namespace cent
+
+#endif
