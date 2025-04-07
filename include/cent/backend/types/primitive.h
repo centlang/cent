aa6dd983289582d41bbb5e8bde3ff481fb3ed1fd
lnkrr <lnkrr@proton.me>
2025-04-07 16:53:33 +0300

    feat(llvm): add i8, i16, i64, and f64

--- include/cent/backend/types/primitive.h
+++ include/cent/backend/types/primitive.h
@@ -5,12 +5,15 @@
 
 namespace cent::backend::types {
 
+struct I8 : detail::Type<I8> {};
+struct I16 : detail::Type<I16> {};
 struct I32 : detail::Type<I32> {};
+struct I64 : detail::Type<I64> {};
 
 struct F32 : detail::Type<F32> {};
+struct F64 : detail::Type<F64> {};
 
 struct Bool : detail::Type<Bool> {};
-
 struct Void : detail::Type<Void> {};
 
 } // namespace cent::backend::types
